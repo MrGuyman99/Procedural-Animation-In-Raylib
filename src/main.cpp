ee38a981461aafde4ea6ec9514d56da51bc47a5c
MrGuyman99 <bodiepagelow@gmail.com>
2025-10-20 19:45:59 -0500

    Added more Circles

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ int main(){
     
     LeaderCircle Circle = LeaderCircle(100, raylib::Vector2(500, 400), "Leader Circle Window");
     Follow_Circle FCircle(75, raylib::Vector2(400, 300), "Follow Circle Window");
+    Follow_Circle FCircle2(50, raylib::Vector2(300, 200), "Follow Circle2 Window");
     raylib::Vector2 Point(500, 400);
 
     while(window.ShouldClose() == false){
@@ -20,17 +21,20 @@ int main(){
             
             window.ClearBackground(DARKGRAY);
             FCircle.Position = Circle.Update(FCircle.Position);
-            Point = FCircle.Update(Point);
+            FCircle2.Position = FCircle.Update(FCircle2.Position);
+            Point = FCircle2.Update(Point);
             Circle.Draw();
             FCircle.Draw();
+            FCircle2.Draw();
             //ONLY Call for Leader circle
             Circle.Control();
             
-            Point.DrawCircle(50, PURPLE);
+            Point.DrawCircle(25, PURPLE);
             DrawFPS(3, 3);
             rlImGuiBegin();
                 Circle.RenderUI();
                 FCircle.RenderUI();
+                FCircle2.RenderUI();
             rlImGuiEnd();
         EndDrawing();
     }
